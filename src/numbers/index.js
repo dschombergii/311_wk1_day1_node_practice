772ace86417981d104008101c9e5805280be8ba8
dschombergii <d.schombergii@gmail.com>
2020-11-24 21:48:09 -0600

    initial commit

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,29 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 == 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+  return sum
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let i in arr) {
+    for (let j in arr) {
+      if (i + j == sum) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,18 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  let newStr = str.split(delim)
+  return newStr
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  return str.match(/..?/g)
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let splitArr = str.split("")
+  return str.split("").reverse().join("")
 }
 
 module.exports = {
